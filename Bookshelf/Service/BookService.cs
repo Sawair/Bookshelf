4176980e2b8fb5f7375aa79b56cec1ad5598c63c
Mariusz Sawair Czerkies <czerkies2805@gmail.com>
2019-09-28 21:08:09 +0200

    Adds possibility to add new book

--- Bookshelf/Service/BookService.cs
+++ Bookshelf/Service/BookService.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Bookshelf.Data;
+using Bookshelf.Data.Model;
 using Bookshelf.ViewModel;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Bookshelf.Service
 {
@@ -32,6 +35,51 @@ namespace Bookshelf.Service
                     Tags = b.Tags.Select(t => t.Tag.Name).ToList()
                 })
                 .ToArrayAsync();
+        }
+
+        public async Task AddBookViewModel(BookViewModel bookViewModel)
+        {
+            var tags = bookViewModel.Tags.Select(t => t.ToLower().Trim()).Distinct().ToList();
+            var newTags = tags.Where(t => !_context.Tag.Any(tag => tag.Name == t)).ToList();
+
+            await using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    foreach (var tag in newTags)
+                    {
+                        _context.Tag.Add(new Tag() { Name = tag });
+                    }
+
+                    await _context.SaveChangesAsync();
+                    
+                    var bookTags = await _context
+                        .Tag
+                        .Where(t => tags.Contains(t.Name))
+                        .Select(t => new BookTag { Tag = t })
+                        .ToListAsync();
+
+                    _context.Book.Add(new Book
+                    {
+                        Title = bookViewModel.Title,
+                        IsEbook = bookViewModel.IsEbook,
+                        Available = bookViewModel.Available,
+                        Tags = bookTags
+                    });
+
+                    await _context.SaveChangesAsync();
+
+                    await transaction.CommitAsync();
+                }
+                catch (Exception)
+                {
+                    await transaction.RollbackAsync();
+                    throw;
+                }
+            }
+
+
+
         }
     }
 }
